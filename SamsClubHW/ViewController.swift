dbb25c46453a9c1ac351ce20a52064c856716f07
Michael <realq86@gmail.com>
2017-09-22 10:54:50 -0700

    Initial Commit

---
+++ SamsClubHW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamsClubHW
+//
+//  Created by Chi Hwa Michael Ting on 9/22/17.
+//  Copyright © 2017 Chi Hwa Michael Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
