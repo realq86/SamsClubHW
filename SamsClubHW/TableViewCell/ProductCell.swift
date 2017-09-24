a60d0709b42703823704592404ad3b5e4aeaacdb
Michael <realq86@gmail.com>
2017-09-23 21:58:35 -0700

    Create basic tableView cell layout to display basic name.

---
+++ SamsClubHW/TableViewCell/ProductCell.swift
@@ -0,0 +1,32 @@
+//
+//  ProductCell.swift
+//  SamsClubHW
+//
+//  Created by Chi Hwa Michael Ting on 9/23/17.
+//  Copyright © 2017 Chi Hwa Michael Ting. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    var viewModel: ProductCellViewModelProtocol! {
+        didSet {
+            self.titleLabel.text = viewModel.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
