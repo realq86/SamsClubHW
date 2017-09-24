5b25b947825b8a74d994e1f2b0dfae417d01b026
Michael <realq86@gmail.com>
2017-09-23 21:57:03 -0700

    Complete json mapping to local Product model

---
+++ SamsClubHW/Model/SamProduct.swift
@@ -0,0 +1,49 @@
+//
+//  SamProduct.swift
+//  SamsClubHW
+//
+//  Created by Chi Hwa Michael Ting on 9/23/17.
+//  Copyright © 2017 Chi Hwa Michael Ting. All rights reserved.
+//
+
+import Foundation
+
+protocol SamProductModelProtocol {
+    var name: String { get }
+}
+
+class SamProduct: SamProductModelProtocol {
+    
+    var productID: String
+    var name: String
+    var shortDescrip: NSAttributedString
+    var longDescrip: NSAttributedString
+    var price: String
+    var imageURL: URL?
+    var inStock: Bool
+    var reviewRating: Float?
+    var reviewCount: Int
+    
+    init(_ dic:[String: AnyObject]) {
+        
+        productID = dic[kProductID] as? String ?? ""
+        name = dic[kProductName] as? String ?? ""
+        
+        let shortDescripStr = dic[kShortDescription] as? String ?? ""
+        shortDescrip = StringFrom(html: shortDescripStr)
+        
+        let longDescripStr = dic[kLongDescription] as? String ?? ""
+        longDescrip = StringFrom(html: longDescripStr)
+        
+        price = dic[kPrice] as? String ?? "n\\a"
+        
+        imageURL = URL(string: dic[kImage] as? String ?? "")
+        
+        inStock = dic[kInStock] as? Bool ?? false
+        
+        reviewCount = dic[kReviewCount] as? Int ?? 0
+        if reviewCount > 0 {
+            reviewRating = dic[kReviewRating] as? Float ?? 0.0
+        }
+    }
+}
