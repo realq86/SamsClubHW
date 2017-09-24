efdf9892ec10f2a511c607cfada4b0949388c846
Michael <realq86@gmail.com>
2017-09-23 21:59:43 -0700

    Embed first VC in Navigation controller and set dependency in the app delegate.

--- SamsClubHW/AppDelegate.swift
+++ SamsClubHW/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let rootNaviVC = self.window?.rootViewController as! UINavigationController
+        
+        let rootVC = rootNaviVC.viewControllers[0] as! ProductListVC
+        
+        rootVC.viewModel = ProductListViewModel(dataProvider: SamServer.shared)
+        
         return true
     }
 
